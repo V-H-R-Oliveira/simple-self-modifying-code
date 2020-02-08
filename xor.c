854d7deebc84808438c2c24271a1ab5d9860e325
V-H-R-Oliveira <vitor.oliveira.24.24@gmail.com>
2020-02-08 20:27:16 -0300

    Bug correction on mprotect size

--- xor.c
+++ xor.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/mman.h>
 #define SIZE 20
 
-void changeProtect(void *addr)
+void changeProtect(void *addr, size_t memSize)
 {
     int pagesize = getpagesize();
     addr -= ((unsigned long)addr % pagesize);
-
-    if (mprotect(addr, pagesize, PROT_EXEC | PROT_READ | PROT_WRITE) == -1)
+    
+    if (mprotect(addr, memSize, PROT_EXEC | PROT_READ | PROT_WRITE) == -1)
     {
         perror("mprotec error:");
         exit(EXIT_FAILURE);
@@ -31,8 +32,9 @@ void runtime_patching(void *addr, size_t size, int key)
     for (unsigned char offset = 0; offset < size; ++offset)
     {
         instruction = (unsigned char *)addr+offset;
+
         if (offset == 45) {
-            unsigned char *toPatch = (unsigned char *)addr+offset;
+            unsigned char *toPatch = instruction;
             *toPatch = key;
         }
     }
@@ -41,28 +43,23 @@ void runtime_patching(void *addr, size_t size, int key)
 int main(void)
 {
     unsigned char plaintext[] = "Self-modifying-code";
+    size_t memSize = (void *)runtime_patching - (void *)xor_obfuscation;
+    changeProtect(xor_obfuscation, memSize);
     
-    changeProtect(xor_obfuscation);
-    puts("The original string:");
-    puts(plaintext);
-    
-    puts("After the first layer of obfuscation:");
+    puts("Before patching:");
+    printf("%s\n", plaintext);
     xor_obfuscation(plaintext);
-    puts(plaintext);
-    runtime_patching(xor_obfuscation, (void *)runtime_patching - (void *)xor_obfuscation, 0x3f);
+    printf("%s\n", plaintext);
+    runtime_patching(xor_obfuscation, memSize, 0x3f);
     
-    puts("After patching the key:");
+    puts("After patching:");
     xor_obfuscation(plaintext);
-    puts(plaintext);
-    
-    puts("After removing the second layer of obfuscation:");
+    printf("%s\n", plaintext);
     xor_obfuscation(plaintext);
-    puts(plaintext);
-    
-    puts("The original string again:");
-    runtime_patching(xor_obfuscation, (void *)runtime_patching - (void *)xor_obfuscation, 0x1f);
+    printf("%s\n", plaintext);
+    runtime_patching(xor_obfuscation, memSize, 0x1f);
     xor_obfuscation(plaintext);
-    puts(plaintext);
+    printf("%s\n", plaintext);
     
     return 0;
 }
